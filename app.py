8a7c4f4426f12524217726318d272d802dd72de9
Ryan Zhang <rzjournal@rzjournal.com>
2023-11-15 22:27:56 -0500

    app

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request, jsonify
+from flask_wtf import FlaskForm
+from wtforms import StringField, DateField
+from wtforms.validators import InputRequired
+import datetime
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret_key'
+
+class TaskForm(FlaskForm):
+    name = StringField('Task Name', validators=[InputRequired()])
+    date = DateField('Task Date', format='%Y-%m-%d', validators=[InputRequired()])
+
+tasks = [
+    {"id": 1, "title": "Event 1", "start": "2023-11-15"},
+    {"id": 2, "title": "Event 2", "start": "2023-11-18"}
+]
+
+@app.route('/')
+def index():
+    return render_template('calendar.html', tasks=tasks)
+
+@app.route('/add_task', methods=['POST'])
+def add_task():
+    form = TaskForm(request.form)
+    if form.validate():
+        task = {
+            "id": len(tasks) + 1,
+            "title": form.name.data,
+            "start": form.date.data.strftime('%Y-%m-%d')
+        }
+        tasks.append(task)
+        return jsonify({"success": True, "message": "Task added successfully"})
+    else:
+        return jsonify({"success": False, "errors": form.errors})
+
+if __name__ == '__main__':
+    app.run(debug=True)
